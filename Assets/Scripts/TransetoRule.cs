60ecef01dfa57288a8e5680053d600b596300aca
Eito-kashi <al23018@shibaura-it.ac.jp>
2025-07-06 22:58:53 +0900

    Rule Button, Scene

---
+++ Assets/Scripts/TransetoRule.cs
@@ -0,0 +1,21 @@
+// Assets/Scripts/SceneManagement/GameSceneManager.cs
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン管理のための名前空間
+
+public class TransetoRule : MonoBehaviour
+{
+    // Inspector から設定するボタンなどがあればここに public 変数を追加
+
+    /// <summary>
+    /// ルール説明シーンをロードします。
+    /// </summary>
+    public void GoToRuleExplanation()
+    {
+        // シーンの名前でロード
+        SceneManager.LoadScene("Rule");
+        // もしくは、Build Settingsでのインデックスでロード
+        // SceneManager.LoadScene(1); // 例: RuleExplanationSceneが2番目 (インデックス1) の場合
+    }
+
+    // その他のゲームシーンのロジック...
+}
